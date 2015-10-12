4f2008775d45af971fa0050cbc01196ef461f137
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-10-12 15:17:42 +0100

    set up restaurant resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+    resources :restaurants
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
